b39658097a44ab3bc4615e398656fc2db9801540
Julian Poyourow <julianpoyo@gmail.com>
2020-03-25 22:27:49 -0700

    Move badWords to constants

---
+++ src/constants/badWords.js
@@ -0,0 +1,35 @@
+export const generalBadWords = [
+  'instructions',
+  'directions',
+  'procedure',
+  'method',
+  'you will need',
+  'how to make it',
+  'ingredients',
+  'total time',
+  'active time',
+  'prep time',
+  'time',
+  'yield',
+  'servings',
+  'notes',
+  'select all ingredients',
+  'select all'
+];
+export const allRecipesBadWords = [
+  'decrease serving',
+  'increase serving',
+  'adjust',
+  'the ingredient list now reflects the servings specified',
+  'footnotes'
+];
+export const tastyRecipesBadWords = [
+  'scale 1x2x3x'
+];
+
+export const badWords = [
+  generalBadWords,
+  allRecipesBadWords,
+  tastyRecipesBadWords
+].flat();
+
---
+++ src/constants/badWords.spec.js
@@ -0,0 +1,31 @@
+import {
+  badWords,
+  generalBadWords,
+  allRecipesBadWords,
+  tastyRecipesBadWords,
+} from './badWords';
+
+describe('badWords', () => {
+  it('matches snapshot', () => {
+    expect(badWords).toMatchSnapshot()
+  })
+})
+
+describe('generalBadWords', () => {
+  it('matches snapshot', () => {
+    expect(generalBadWords).toMatchSnapshot()
+  })
+})
+
+describe('allRecipesBadWords', () => {
+  it('matches snapshot', () => {
+    expect(allRecipesBadWords).toMatchSnapshot()
+  })
+})
+
+describe('tastyRecipesBadWords', () => {
+  it('matches snapshot', () => {
+    expect(tastyRecipesBadWords).toMatchSnapshot()
+  })
+})
+

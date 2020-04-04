75d4ab7f7e84bafc3da18acdcb44cb79dead0221
Julian Poyourow <julianpoyo@gmail.com>
2020-04-04 10:18:41 -0700

    chore(build): Add test:ci command with coverage limits

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
 }
 

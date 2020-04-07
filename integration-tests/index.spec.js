1d031f0e7041297b3c16a3088cce8f412de9c035
Julian Poyourow <julianpoyo@gmail.com>
2020-04-06 19:16:07 -0700

    chore(tests): Add sites from classMatchers

--- integration-tests/index.spec.js
+++ integration-tests/index.spec.js
@@ -1,7 +1,16 @@
 import { clipRecipeFromUrl } from './integration-test-utils'
 
 const sites = [
-  'https://www.bettycrocker.com/recipes/ultimate-chocolate-chip-cookies/77c14e03-d8b0-4844-846d-f19304f61c57'
+  'https://www.bettycrocker.com/recipes/ultimate-chocolate-chip-cookies/77c14e03-d8b0-4844-846d-f19304f61c57',
+  'https://panlasangpinoy.com/leche-flan/',
+  'https://sallysbakingaddiction.com/quiche-recipe/',
+  'https://www.allrecipes.com/recipe/231244/asparagus-mushroom-bacon-crustless-quiche/',
+  'https://www.foodnetwork.com/recipes/paula-deen/spinach-and-bacon-quiche-recipe-2131172',
+  'https://www.delish.com/cooking/recipe-ideas/a25648042/crustless-quiche-recipe/',
+  'https://food52.com/recipes/81867-best-quiche-recipe',
+  'https://myheartbeets.com/paleo-tortilla-chips/',
+  'https://www.bonappetit.com/recipe/chocolate-babka',
+  'https://www.kingarthurflour.com/recipes/chocolate-cake-recipe',
 ];
 
 describe('testing', () => {

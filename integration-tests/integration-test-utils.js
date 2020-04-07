78cd76600d0d3481b1c467325b34716727cbe8c9
Julian Poyourow <julianpoyo@gmail.com>
2020-04-06 19:15:18 -0700

    chore(tests): Exit puppeteer after failed navigation

--- integration-tests/integration-test-utils.js
+++ integration-tests/integration-test-utils.js
@@ -11,24 +11,29 @@ export const clipRecipeFromUrl = async clipUrl => {
 
   await page.setBypassCSP(true);
 
-  await page.goto(clipUrl, {
-    waitUntil: "networkidle2",
-    timeout: 25000
-  });
+  let recipeData;
+  try {
+    await page.goto(clipUrl, {
+      waitUntil: "networkidle2",
+      timeout: 25000
+    });
 
-  await page.evaluate(() => {
-    try {
-      window.scrollTo(0, document.body.scrollHeight);
-    } catch(e) {}
-  });
+    await page.evaluate(() => {
+      try {
+        window.scrollTo(0, document.body.scrollHeight);
+      } catch(e) {}
+    });
 
-  await page.addScriptTag({ path: './dist/recipe-clipper.umd.js' });
-  const recipeData = await page.evaluate(() => {
-    return window.RecipeClipper.clipRecipe();
-  });
+    await page.addScriptTag({ path: './dist/recipe-clipper.umd.js' });
+    recipeData = await page.evaluate(() => {
+      return window.RecipeClipper.clipRecipe();
+    });
+  } catch(e) {
+    await page.close();
+    await browser.close();
 
-  await page.close();
-  await browser.close();
+    throw e;
+  }
 
   return recipeData;
 };

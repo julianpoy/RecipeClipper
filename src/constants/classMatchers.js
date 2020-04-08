86befbca161f8079b98ef980da77f063064e5e0d
Julian Poyourow <julianpoyo@gmail.com>
2020-04-07 21:17:29 -0700

    feat(sites): Add support for BettyCrocker instructions

--- src/constants/classMatchers.js
+++ src/constants/classMatchers.js
@@ -64,6 +64,7 @@ const classMatchers = {
       'recipe-method-section', // Taste.com.au - https://www.taste.com.au/recipes/healthy-feta-mint-beef-patties-griled-vegies-hummus-recipe/pxacqmfu?r=recipes/dinnerrecipesfortwo&c=1j53ce29/Dinner%20recipes%20for%20two
 
       'recipe__list--steps', // Food52.com - https://food52.com/recipes/81867-best-quiche-recipe
+      'recipesteps', // BettyCrocker.com - https://www.bettycrocker.com/recipes/ultimate-chocolate-chip-cookies/77c14e03-d8b0-4844-846d-f19304f61c57
       'instructions', // Generic
       'recipe-steps', // Generic
       'recipe-instructions', // Generic

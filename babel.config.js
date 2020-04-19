70efe7f437de0d57188e79ee5a4e3f9c80ecf89c
Julian Poyourow <julianpoyo@gmail.com>
2020-04-19 09:13:13 -0700

    feat(pkg): Add Core.js (for now) for browser and node compat

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,12 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env',
+      {
+        targets: "defaults, maintained node versions",
+        useBuiltIns: "usage",
+        corejs: 3
+      }
     ],
   ],
+  ignore: [/core-js/]
 };

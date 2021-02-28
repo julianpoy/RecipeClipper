552493b30d299159b20f90bb0a7d4ccbc23a8a77
Julian Poyourow <julianpoyo@gmail.com>
2021-02-27 18:48:59 -0800

    WIP

--- src/utils/microdata.js
+++ src/utils/microdata.js
@@ -2,7 +2,7 @@ import * as self from './microdata';
 import { getInnerText } from './innerText';
 import { getLongestString } from './text';
 
-export const getLongestTextForQueries = (window, queries) => {
+export const getLongestSingleElementTextForQueries = (window, queries) => {
   const vals = queries
     .map((query) => [...window.document.querySelectorAll(query)])
     .flat()
@@ -11,24 +11,39 @@ export const getLongestTextForQueries = (window, queries) => {
   return getLongestString(vals);
 };
 
-export const getActiveTimeFromMicrodata = (window) => self.getLongestTextForQueries(window, [
+export const getLongestTextGroupForQueries = (window, queries) => {
+  const vals = queries
+    .map((query) =>
+         [...window.document.querySelectorAll(query)]
+           .map((el) => getInnerText(el))
+           .join('\n');
+    );
+
+  return getLongestString(vals);
+};
+
+export const getActiveTimeFromMicrodata = (window) => self.getLongestSingleElementTextForQueries(window, [
   '[itemProp=prepTime]',
 ]);
 
-export const getTotalTimeFromMicrodata = (window) => self.getLongestTextForQueries(window, [
+export const getTotalTimeFromMicrodata = (window) => self.getLongestSingleElementTextForQueries(window, [
   '[itemProp=totalTime]',
 ]);
 
-export const getYieldFromMicrodata = (window) => self.getLongestTextForQueries(window, [
+export const getYieldFromMicrodata = (window) => self.getLongestSingleElementTextForQueries(window, [
   '[itemProp=recipeYield]',
 ]);
 
-export const getInstructionsFromMicrodata = (window) => self.getLongestTextForQueries(window, [
+export const getInstructionsFromMicrodata = (window) => self.getLongestTextGroupForQueries(window, [
   '[itemProp=recipeInstructions]',
+  '[itemProp=recipeInstruction]',
   '[itemProp=instructions]',
+  '[itemProp=instruction]',
 ]);
 
-export const getIngredientsFromMicrodata = (window) => self.getLongestTextForQueries(window, [
+export const getIngredientsFromMicrodata = (window) => self.getLongestTextGroupForQueries(window, [
   '[itemProp=recipeIngredients]',
+  '[itemProp=recipeIngredient]',
   '[itemProp=ingredients]',
+  '[itemProp=ingredient]',
 ]);
